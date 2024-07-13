45e822259309f7e7f753a3903b315ae0c6d2f864
Adam Kwiatek <kwiatek.adam@poczta.fm>
2024-07-14 00:55:02 +0200

    medium-parse-url-params

--- questions/09616-medium-parse-url-params/template.ts
+++ questions/09616-medium-parse-url-params/template.ts
@@ -1,1 +1,4 @@
-type ParseUrlParams<T> = any
+type ParseUrlParams<S extends string> =
+  S extends `${string}:${infer Param}/${infer Rest}` ? Param | ParseUrlParams<Rest> :
+  S extends `${string}:${infer Param}` ? Param :
+  never;
