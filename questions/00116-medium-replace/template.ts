dd2e7ac25267175cfb8b703be482477ab140cef6
Adam Kwiatek <kwiatek.adam@poczta.fm>
2024-07-13 22:59:52 +0200

    medium-replace

--- questions/00116-medium-replace/template.ts
+++ questions/00116-medium-replace/template.ts
@@ -1,1 +1,1 @@
-type Replace<S extends string, From extends string, To extends string> = any
+type Replace<S extends string, From extends string, To extends string> = From extends '' ? S : S extends `${infer Before}${From}${infer After}` ? `${Before}${To}${After}` : S;
