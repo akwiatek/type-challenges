efd5a49dad3b9ef4ce64132ffa95dab49702085c
Adam Kwiatek <kwiatek.adam@poczta.fm>
2024-07-14 22:21:32 +0200

    medium-number-range

--- questions/08640-medium-number-range/template.ts
+++ questions/08640-medium-number-range/template.ts
@@ -1,1 +1,17 @@
-type NumberRange<L, H> = any
+type Series<
+  WantedLength extends number,
+  Previous extends {
+    tuple: unknown[],
+    payload: unknown,
+  } = {
+    tuple: [],
+    payload: never,
+  },
+  CurrentLength = Previous['tuple']['length'],
+  Payload = Previous['payload'] | CurrentLength
+  > =
+  WantedLength extends CurrentLength
+    ? Payload
+    : Series<WantedLength, {tuple: [...Previous['tuple'], unknown], payload: Payload}>;
+
+type NumberRange<L extends number, H extends number> = L | Exclude<Series<H>, Series<L>>;
