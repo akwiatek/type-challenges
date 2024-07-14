75f626b5416a70ead4821bbb7282f48674d09668
Adam Kwiatek <kwiatek.adam@poczta.fm>
2024-07-14 21:47:25 +0200

    medium-lastindexof

--- questions/05317-medium-lastindexof/template.ts
+++ questions/05317-medium-lastindexof/template.ts
@@ -1,1 +1,11 @@
-type LastIndexOf<T, U> = any
+type Same<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
+
+type LastIndexOf<T extends unknown[], U> =
+  T extends [...infer Before, infer Last]
+    ? Same<Last, U> extends true
+      ? Before['length']
+      : LastIndexOf<Before, U>
+    : -1
+;
