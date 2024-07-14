fa25565943dab1acbdccc2c6d45050cfae9d78cf
Adam Kwiatek <kwiatek.adam@poczta.fm>
2024-07-14 20:43:57 +0200

    medium-join

--- questions/05310-medium-join/template.ts
+++ questions/05310-medium-join/template.ts
@@ -1,1 +1,5 @@
-type Join<T, U> = any
+type Join<T, U extends string | number = ','> =
+  T extends [] ? '' :
+  T extends [infer Only extends string] ? `${Only}` :
+  T extends [infer First extends string, ...infer Rest] ? `${First}${U}${Join<Rest, U>}` :
+  never
