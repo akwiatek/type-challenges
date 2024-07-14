5265471074bb7d52c87283e509cda1f16ab2c112
Adam Kwiatek <kwiatek.adam@poczta.fm>
2024-07-14 23:31:31 +0200

    medium-checkrepeatedchars

--- questions/09142-medium-checkrepeatedchars/template.ts
+++ questions/09142-medium-checkrepeatedchars/template.ts
@@ -1,1 +1,6 @@
-type CheckRepeatedChars<T extends string> = any
+type CheckRepeatedChars<T extends string, PreviousCharacters = never> =
+  T extends `${infer First}${infer Remaining}`
+    ? First extends PreviousCharacters
+      ? true
+      : CheckRepeatedChars<Remaining, PreviousCharacters | First>
+    : false;
