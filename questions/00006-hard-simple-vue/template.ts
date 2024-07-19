7ccf2e41036491e60b3c651f2942c6760c5342f2
Adam Kwiatek <kwiatek.adam@poczta.fm>
2024-07-20 00:02:18 +0200

    hard-simple-vue

--- questions/00006-hard-simple-vue/template.ts
+++ questions/00006-hard-simple-vue/template.ts
@@ -1,1 +1,17 @@
-declare function SimpleVue(options: any): any
+declare function SimpleVue<
+  Data,
+  Computed,
+  Methods,
+  ThisOutcome =
+    & Data
+    & {
+      [P in keyof Computed]: Computed[P] extends (this: Data) => infer R ? R : never;
+    }
+    & Methods
+  >(
+    options: {
+      data: (this: void) => Data,
+      computed: Computed,
+      methods: Methods,
+    } & ThisType<ThisOutcome>
+  ): ThisOutcome
