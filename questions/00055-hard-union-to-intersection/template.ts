3843c71ceb217d90365c2983ceb25849d4e17aa0
Adam Kwiatek <kwiatek.adam@poczta.fm>
2024-07-19 20:56:10 +0200

    hard-union-to-intersection

--- questions/00055-hard-union-to-intersection/template.ts
+++ questions/00055-hard-union-to-intersection/template.ts
@@ -1,1 +1,3 @@
-type UnionToIntersection<U> = any
+type Consumers<U> = U extends unknown ? (arg: U) => never : never;
+
+type UnionToIntersection<U> = Consumers<U> extends (arg: infer CommonArg) => never ? CommonArg : never;
