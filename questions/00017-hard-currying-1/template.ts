b213eae492433bec929f7089e4225ba108fb8956
Adam Kwiatek <kwiatek.adam@poczta.fm>
2024-07-28 21:54:04 +0200

    hard-currying-1

--- questions/00017-hard-currying-1/template.ts
+++ questions/00017-hard-currying-1/template.ts
@@ -1,1 +1,8 @@
-declare function Currying(fn: any): any
+type Curried<F> =
+  F extends (...args: infer T) => infer R ?
+    T['length'] extends 0 | 1 ? F :
+    T extends [infer A, ...infer Rest] ? (a: A) => Curried<(...rest: Rest) => R> :
+    never :
+  never;
+
+declare function Currying<F>(fn: F): Curried<F>;
